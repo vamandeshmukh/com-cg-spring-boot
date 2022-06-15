73732ac7311f811afcb9bb4e6ce907308df67a90
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-15 14:07:32 +0530

    springboot

--- src/main/java/com/cg/spring/boot/controller/EmployeeController.java
+++ src/main/java/com/cg/spring/boot/controller/EmployeeController.java
@@ -1,15 +1,19 @@
 package com.cg.spring.boot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.cg.spring.boot.model.Employee;
 
+@RestController
 public class EmployeeController {
 
 //	http://localhost:9999/getemp
 
+	@GetMapping("/getemp")
 	public Employee getEmp() {
-
-		return null;
-
+		Employee emp = new Employee(101, "Sonu", 50000);
+		System.out.println(emp.toString());
+		return emp;
 	}
-
 }
