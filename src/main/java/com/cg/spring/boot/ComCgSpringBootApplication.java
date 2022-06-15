6cb254b443ddae6682f638bf99457618c782110b
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-15 12:30:56 +0530

    springboot

--- src/main/java/com/cg/spring/boot/ComCgSpringBootApplication.java
+++ src/main/java/com/cg/spring/boot/ComCgSpringBootApplication.java
@@ -3,6 +3,14 @@ package com.cg.spring.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Vaman Deshmukh
+ * 
+ *         {@link https://github.com/vamandeshmukh/com-cg-spring-boot}
+ *
+ */
+
 @SpringBootApplication
 public class ComCgSpringBootApplication {
 
--- src/main/java/com/cg/spring/boot/controller/Hello.java
+++ src/main/java/com/cg/spring/boot/controller/Hello.java
@@ -6,10 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
+//	http://localhost:9999/hello
+
 	@GetMapping("/hello")
 	public String hello() {
 		System.out.println("Hello");
 		return "Hello world!";
 	}
 
+	@GetMapping("/hi")
+	public String hi() {
+		System.out.println("hi");
+		return "Hi buddies!";
+	}
 }
