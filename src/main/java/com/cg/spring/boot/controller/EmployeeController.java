de169581f72b85a6213ade0748e435d83cda2314
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-15 12:49:13 +0530

    springboot

---
+++ src/main/java/com/cg/spring/boot/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.cg.spring.boot.controller;
+
+import com.cg.spring.boot.model.Employee;
+
+public class EmployeeController {
+
+//	http://localhost:9999/getemp
+
+	public Employee getEmp() {
+
+		return null;
+
+	}
+
+}
---
+++ src/main/java/com/cg/spring/boot/model/Employee.java
@@ -0,0 +1,5 @@
+package com.cg.spring.boot.model;
+
+public class Employee {
+
+}
